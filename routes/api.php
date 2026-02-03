ff0473c38350c565212a1665a369004cde4e5ee9
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-02-02 23:38:49 -0600

    Add endpoint versioning

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\PhotoController;
 
-Route::get('/places', [PlaceController::class, 'index']);
+Route::prefix('v1')->group(function (){
+    Route::get('/places', [PlaceController::class, 'index']);
 
-Route::post('/places', [PlaceController::class, 'store']);
-Route::post('/photos/upload', [PhotoController::class, 'upload']);
+    Route::post('/places', [PlaceController::class, 'store']);
+    Route::post('/photos/upload', [PhotoController::class, 'upload']);
+});
