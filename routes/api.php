0ca475d8a38e7b3f4d3a939190b41e51e6378a83
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-02-04 20:27:04 -0600

    Install tymon/jwt-auth settings

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\AuthController;
 
 Route::prefix('v1')->group(function (){
     Route::get('/places', [PlaceController::class, 'index']);
@@ -14,4 +15,14 @@ Route::prefix('v1')->group(function (){
     
     Route::put('/places/{place}', [PlaceController::class, 'update']);
     Route::delete('/places/{place}', [PlaceController::class, 'destroy']);
+
+    Route::group([
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    ], function ($router) {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::post('/me', [AuthController::class, 'me']);
+    });
 });
