2e0f1ecf8907ce673619738bfc542a8455b8c312
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-02-07 14:31:24 -0600

    Add role protection to admin endpoints with 'Spatie'

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\ChangeRequestController;
 
 Route::prefix('v1')->group(function (){
     Route::get('/places', [PlaceController::class, 'index']);
 
-    Route::group([
-        'middleware' => 'auth:api'
-    ],function ($router){
+    Route::group(['middleware' => 'auth:api'],function ($router){
         Route::post('/places', [PlaceController::class, 'store']);
-        Route::post('/photos/upload', [PhotoController::class, 'upload']);
-            
         Route::put('/places/{place}', [PlaceController::class, 'update']);
         Route::delete('/places/{place}', [PlaceController::class, 'destroy']);
+        
+        Route::post('/photos/upload', [PhotoController::class, 'upload']);
+
+        Route::middleware(['role:admin'])->prefix('admin')->group(function(){
+            Route::get('/requests', [ChangeRequestController::class, 'index']);
+        });
     });
 
-    Route::group([
-        'middleware' => 'api',
-        'prefix' => 'auth'
-    ], function ($router) {
+    Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/logout', [AuthController::class, 'logout']);
